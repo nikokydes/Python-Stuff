8b232fc8bfb830ff699e6325eb676ff06847d9cd
Nicolaos Kydes <nkydes@onlocationinc.com>
2016-10-19 17:26:22 -0400

    Made more generic
    convertCrosstab.py now can handle an arbitrary number of non-crosstab
    columns before the yearly columns.

--- utilities/convertCrosstab.py
+++ utilities/convertCrosstab.py
@@ -3,27 +3,40 @@ import csv
 import optparse
 import string
 
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
 # Create a flat file from a cross tab file
-def convertCT(inputFile,outputFile):
+def convertCT(inputFile,outputFile,CTcol):
 
-	inFile  = open(inputFile, 'rt')
-	reader = csv.DictReader(inFile)
-	input_data = list(reader)
-	inFile.close()
+    inFile  = open(inputFile, 'rt')
+    reader = csv.DictReader(inFile)
+    input_data = list(reader)
+    inFile.close()
 
-	outFile = open(outputFile,'wb')
-	writer = csv.writer(outFile)
-	writer.writerow(['RowLabel','ColumnLabel','Value']) 
+    outFile = open(outputFile,'wb')
+    writer = csv.writer(outFile)
 
-	# Get the unique set of column headers
-	col_keys = list(set([m for m in input_data[0].keys() if m.strip()!='label']))
+    # Get the unique set of column headers
+    col_keys    = sorted(list(set([x for x in input_data[0].keys() if is_number(x)])))
+    col_headers = sorted(list(set([x for x in input_data[0].keys() if not is_number(x)])))
 
-	for i in input_data:
-		for j in col_keys:
-			if i[j].strip(): 
-				writer.writerow([i['label'],j,"{:.10f}".format(float(i[j]))])
+    writer.writerow(col_headers + [CTcol,'Value'])
 
-	outFile.close()
+    for i in input_data:
+        xx = [i[x] for x in col_headers]
+        for j in col_keys:
+            if i[j].strip(): 
+                yy = xx + [j] + ["{:.10f}".format(float(i[j]))]
+                writer.writerow(yy)    
+
+    outFile.close()
+
 
 # Create a cross tab file from a flat file
 def createCT(inputFile,outputFile,sourceHeaders):
@@ -80,6 +93,7 @@ def main(argv=None):
       """)
     parser.add_option("-i", "--input", help="Name of input CSV file in crosstab format", default=None)
     parser.add_option("-o", "--output", help="Name of output CSV file", default=None)
+    parser.add_option("-c", "--CTcol", help="Label of the crosstab column set (i.e. year)", default=None)
     parser.add_option("-f", "--flat2CT", help="Convert from flat file to cross tab", action="store_true", dest="flat2CT")
     parser.add_option("-l", "--labels", help="Header labels to pull data from in input CSV (row,col,value)", default=None)
 
@@ -92,15 +106,20 @@ def main(argv=None):
             sys.exit()
     else:
         sourceHeaders = []
+
+    if options.CTcol:
+        CTcol = options.CTcol.trim()
+    else:
+        CTcol = 'year'
  
     # Download new EIA data files if requested
     if options.input and options.output:
         if options.flat2CT:
-        	createCT(options.input,options.output,sourceHeaders)
+            createCT(options.input,options.output,sourceHeaders)
         else:
-            convertCT(options.input,options.output)
+            convertCT(options.input,options.output,CTcol)
     else:
-    	print "Both an input and output file must be specified, exiting.."
+        print "Both an input and output file must be specified, exiting.."
 
 
 if __name__ == "__main__":
